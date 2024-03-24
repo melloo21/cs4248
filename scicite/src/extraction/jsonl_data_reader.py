3c11914afe8f3fad270347cfd9b1080c9041a837
meow-bot94 <meow_bot@hotmail.com>
2024-03-24 20:01:36 +0800

    Adding initial utility files

---
+++ scicite/src/extraction/jsonl_data_reader.py
@@ -0,0 +1,21 @@
+import json
+from pathlib import Path
+
+from src.extraction.data_instance import DataInstance
+from src.utils.path_getter import PathGetter
+
+
+class JsonlDataReader:
+    def __init__(self, file_path: Path = None):
+        self._path = file_path or PathGetter.get_data_directory() / 'dev.jsonl'
+
+    def read(self) -> dict[str, DataInstance]:
+        with open(self._path, 'r', encoding='utf8') as f:
+            data = [json.loads(file_line) for file_line in f]
+        data_instances = [
+            DataInstance(
+                row['string'], row['label'], row['id'], row['citeStart'], row['citeEnd']
+            )
+            for row in data
+        ]
+        return {data.id: data for data in data_instances}
