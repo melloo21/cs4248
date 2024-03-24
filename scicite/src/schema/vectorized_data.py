d3039f7fa5a991452b9407ec882efc8d7af4cb5c
meow-bot94 <meow_bot@hotmail.com>
2024-03-25 01:14:37 +0800

    [fix] fix VectorizedData for sparse matrices

--- scicite/src/schema/vectorized_data.py
+++ scicite/src/schema/vectorized_data.py
@@ -14,8 +14,9 @@ class VectorizedData:
     label_indices: Collection[LabelIndices] = field(init=False)
 
     def __post_init__(self):
-        assert len(self.vectors) == len(self.id) == len(self.labels), (
+        vector_length = self.vectors.shape[0] if hasattr(self.vectors, 'shape') else len(self.vectors)
+        assert vector_length == len(self.id) == len(self.labels), (
             f'Inputs do not have the same data instance counts: '
-            f'{len(self.vectors)}, {len(self.id)}, {len(self.labels)}'
+            f'{vector_length}, {len(self.id)}, {len(self.labels)}'
         )
         self.label_indices = map_label_string_to_indices(self.labels)
