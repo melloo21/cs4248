f24e685aa0d1d368f9a430f9e77564f8f7e7dd3c
meow-bot94 <meow_bot@hotmail.com>
2024-04-14 17:48:10 +0800

    [feature] add latest attempts at POS tagging features

---
+++ scicite/src/tokenize/spacy_pos_tokenizer.py
@@ -0,0 +1,18 @@
+import spacy
+
+from src.schema.documents import Documents
+from src.schema.tokenized_data import TokenizedData
+from src.tokenize.abstract_tokenizer import AbstractTokenizer
+
+
+class SpacyPosTokenizer(AbstractTokenizer):
+
+    def tokenize(self, document: Documents) -> TokenizedData:
+        nlp = spacy.load('en_core_web_sm', disable=[])
+        tokenized_docs = document.texts
+        tokenized_docs = [
+            [token.pos_ for token in doc]
+            for doc in nlp.pipe(tokenized_docs)
+        ]
+
+        return TokenizedData(tokenized_docs, document.id, document.labels)
