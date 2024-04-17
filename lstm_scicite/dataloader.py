a5a98ee8a8b8c2c6cdc9db8b650091e868977f69
shouting-lu <59173651+shouting-lu@users.noreply.github.com>
2024-04-17 20:41:46 +0800

    change to bert bpe tokenizer

--- lstm_scicite/dataloader.py
+++ lstm_scicite/dataloader.py
@@ -6,44 +6,20 @@ from collections import Counter
 from tqdm import tqdm
 from torch.nn.utils.rnn import pad_sequence
 from config import *
+from transformers import BertTokenizer
+import re
+import pandas as pd
 
-
-# Define Byte Pair Encoding (BPE) tokenizer
-class BPETokenizer:
-    def __init__(self, vocab_size=10000):
-        self.vocab_size = vocab_size
-        self.tokenizer = None
-
-    def fit(self, texts):
-        # Perform BPE tokenization
-        vocab = Counter()
-        for text in texts:
-            vocab.update(text.split())
-        self.tokenizer = {word: idx for idx, (word, _) in enumerate(vocab.most_common(self.vocab_size))}
-        self.tokenizer['<unk>'] = self.vocab_size  # unknown token
-        self.tokenizer['<pad>'] = self.vocab_size + 1  # padding token
-
-    def encode(self, text):
-        if self.tokenizer is None:
-            raise ValueError("Tokenizer not fitted yet!")
-        tokens = [self.tokenizer.get(token, self.tokenizer['<unk>']) for token in text.split()]
-        return tokens
-
-# Define dataset class
 class TextDataset(Dataset):
     def __init__(self, data_path, tokenizer):
         self.data_path = data_path
         self.tokenizer = tokenizer
         self.data = self.load_data()
-        self.fit_tokenizer([item['string'] for item in self.data])
 
     def load_data(self):
         with open(self.data_path, 'r') as file:
             data = [json.loads(line.strip()) for line in file]
         return data
-
-    def fit_tokenizer(self, texts):
-        self.tokenizer.fit(texts)
 
     def __len__(self):
         return len(self.data)
@@ -52,17 +28,23 @@ class TextDataset(Dataset):
         item = self.data[idx]
         text = item['string']
         label = item['label']
-        encoded_text = self.tokenizer.encode(text)
+
+        # text = re.sub(r'\d+(\.\d+)?', '<NUM>', text)
+        # if pd.notna(item['citeStart']) and pd.notna(item['citeEnd']):
+        #     text = text[:int(item['citeStart'])] + "<CITATION>" + text[int(item['citeEnd']):]
+
+        encoded_text = self.tokenizer.encode(text, add_special_tokens=True)
         return encoded_text, label
+
+# Initialize BPE tokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+vocab_size = tokenizer.vocab_size  # 从tokenizer获取词汇表大小
+
 
 def collate_fn(batch):
     inputs, labels = zip(*batch)
-    inputs = [torch.tensor(seq, dtype=torch.float32) for seq in inputs]
-    inputs = pad_sequence(inputs, batch_first=True, padding_value=tokenizer.vocab_size+1)  # Padding
+    inputs = [torch.tensor(seq, dtype=torch.long) for seq in inputs]
+    inputs = pad_sequence(inputs, batch_first=True, padding_value=tokenizer.pad_token_id)
+    # inputs = inputs.to(torch.float)
     labels = torch.tensor([0 if label == 'background' else 1 if label == 'method' else 2 for label in labels])
     return inputs, labels
-
-
-# Initialize BPE tokenizer
-tokenizer = BPETokenizer(vocab_size=vocab_size)
-
